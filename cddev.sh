c0ff4a13c2ca184c20194536bc31d4d2f990c653
karsh <utkarsh@cyberfear.com>
2024-04-29 14:23:31 +1000

    all of a type
    added extension to the script to open all files of a certain type in
    $EDITOR. it'd be useful when project contains a lot of non-code files
    that populate telescope. with this script they'd all be opened in
    buffer.

--- cddev.sh
+++ cddev.sh
@@ -22,5 +22,28 @@ case "$1" in
     main)
         $EDITOR $(find $PROJECT_NAME -type d -name .git -prune -o -iname "main*" -print)
         ;;
+
+    alltype) 
+        # open all project codefile in buffer. comes handy with telescope buffer when new
+        # files are not git commited, or when git commits also consists of a lot of
+        # binaries and non-code files
+        if [ -z "$2" ]; then
+            echo -e "Usage: \e[33mcode \e[34m<file_extensions>"
+            exit 1
+        fi
+
+        # Shift the parameters to access $3, $4, and so on
+        shift
+
+        # Build the find command dynamically based on the provided file extensions
+        find_cmd="find $PROJECT_NAME -type f"
+        for extension in "$@"; do
+            find_cmd+=" -iname '*.$extension' -o"
+        done
+        find_cmd=${find_cmd% -o}  # Remove the trailing '-o'
+
+        # Open all files with the specified extensions in the editor
+        $EDITOR $(eval $find_cmd)
+       ;;
 esac
 
