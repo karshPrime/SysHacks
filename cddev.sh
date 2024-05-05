3e9cedc8c80d6d9c7f59bc18d8fae7d0dd46f607
karsh <utkarsh@cyberfear.com>
2024-05-05 13:35:01 +1000

    compatibility
    + added check to ensure commands are used from within git repo
    ~ modified readme command to make non .md readme just as valid
    ~ updated find command for main to avoid .git/ dir

--- cddev.sh
+++ cddev.sh
@@ -1,55 +1,65 @@
 #!/usr/bin/env bash
 
-PROJECT_NAME=$(git rev-parse --show-toplevel)
+if git rev-parse --is-inside-work-tree &> /dev/null; then
+    PROJECT_NAME=$(git rev-parse --show-toplevel)
 
-case "$1" in
-    readme)
-        $EDITOR $PROJECT_NAME/README.md
-        ;;
+    case "$1" in
+        readme)
+            $EDITOR "$PROJECT_NAME/README*"
+            ;;
 
-    gitignore)
-        $EDITOR $PROJECT_NAME/.gitignore
-        ;;
-        
-    gitconfig)
-        $EDITOR $PROJECT_NAME/.git/config
-        ;;
+        gitignore)
+            $EDITOR "$PROJECT_NAME/.gitignore"
+            ;;
 
-    makefile)
-        $EDITOR $PROJECT_NAME/Makefile
-        ;;
+        gitconfig)
+            $EDITOR "$PROJECT_NAME/.git/config"
+            ;;
 
-    main)
-        $EDITOR $(find $PROJECT_NAME -type d -name .git -prune -o -iname "main*" -print)
-        ;;
+        makefile)
+            $EDITOR "$PROJECT_NAME/Makefile"
+            ;;
 
-    alltype) 
-        # open all project codefile in buffer. comes handy with telescope buffer when new
-        # files are not git commited, or when git commits also consists of a lot of
-        # binaries and non-code files
-        if [ -z "$2" ]; then
-            echo -e "Usage: \e[33mcode \e[34m<file_extensions>"
-            exit 1
-        fi
+        main)
+            $EDITOR $(find $PROJECT_NAME -type d -name ".git" -prune -o -type f\
+                -iname "main*" ! -iname "main.o" -print)
+            ;;
 
-        if [ "$2" == "." ]; then
-            WORKDIR=". -maxdepth 1"
-        else
-            WORKDIR="$PROJECT_NAME"
-        fi
+        parent)
+            cd $PROJECT_NAME
+            ;;
 
-        # Shift the parameters to access $3, $4, and so on
-        shift
+        alltype) 
+            # open all project codefile in buffer. comes handy with telescope buffer when
+            # new files are not git commited, or when git commits also consists of a lot
+            # of binaries and non-code files
+            if [ -z "$2" ]; then
+                echo -e "Usage: \e[33mcode \e[34m<file_extensions>"
+                exit 1
+            fi
 
-        # Build the find command dynamically based on the provided file extensions
-        find_cmd="find $WORKDIR -type f"
-        for extension in "$@"; do
-            find_cmd+=" -iname '*.$extension' -o"
-        done
-        find_cmd=${find_cmd% -o}  # Remove the trailing '-o'
+            if [ "$2" == "." ]; then
+                WORKDIR=". -maxdepth 1"
+            else
+                WORKDIR="$PROJECT_NAME"
+            fi
 
-        # Open all files with the specified extensions in the editor
-        $EDITOR $(eval $find_cmd)
-       ;;
-esac
+            # Shift the parameters to access $3, $4, and so on
+            shift
+
+            # Build the find command dynamically based on the provided file extensions
+            find_cmd="find $WORKDIR -type f"
+            for extension in "$@"; do
+                find_cmd+=" -iname '*.$extension' -o"
+            done
+            find_cmd=${find_cmd% -o}  # Remove the trailing '-o'
+
+            # Open all files with the specified extensions in the editor
+            $EDITOR $(eval $find_cmd)
+            ;;
+    esac
+
+else
+    echo -e "\e[31mError: \e[0mThis action requires the project to be within a git repo."
+fi
 
