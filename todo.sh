1baee18850a1d1a0a9b9f7c48e82a21e710f2c6a
karsh <utkarsh@cyberfear.com>
2024-05-13 06:22:38 +1000

    recognise local todo
    + support for saving .todo in current dir, should that be required

--- todo.sh
+++ todo.sh
@@ -1,12 +1,40 @@
 #!/usr/bin/env bash
 
-TODO_PATH="$HOME/Documents/todo/"
+SAVE_PATH="$HOME/Documents/todo/"
 
+# Project Description ---------------------------------------------------------
+
+# Project Path
 if git rev-parse --is-inside-work-tree &> /dev/null; then
     TITLE_PATH=$(git rev-parse --show-toplevel)
 else
     TITLE_PATH=$(pwd)
 fi
 
-$EDITOR $TODO_PATH/"$(basename $TITLE_PATH)".todo
+# Project Name
+PROJECT_NAME=$(basename $TITLE_PATH)
 
+
+# User Commands ---------------------------------------------------------------
+
+# Check for flags
+contains_flag() {
+    local flag="-$1"
+    shift
+    for arg in "$@"; do
+        [[ "$arg" == "$flag" ]] && return 0
+    done
+    return 1
+}
+
+# Create new file if one doesn't already exists
+# default + support for -l
+if [ -n $(find $SAVE_PATH -type f -name "$PROJECT_NAME.todo") ] ||
+    [ -n $(find $TITLE_PATH -type f -name "$PROJECT_NAME.todo") ]; then
+    
+    if contains_flag "l" "$@"; then
+        touch "$TITLE_PATH/$PROJECT_NAME.todo"
+    else
+        touch "$SAVE_PATH/$PROJECT_NAME.todo"
+    fi
+fi
