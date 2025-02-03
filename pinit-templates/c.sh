99d1fac1d77d762ffc2c319e215f062e39627f7f
karsh <utkarsh@cyberfear.com>
2025-02-04 01:44:11 +1100

    feat: zed shaw debug
    + added Debug.h with Zed Shaw's debug macros

--- pinit-templates/c.sh
+++ pinit-templates/c.sh
@@ -12,22 +12,75 @@ echo "
 // main.c
 
 // #include <stdio.h>
-#include \"Aliases.h\"
+#include \"Debug.h\"
 
-int main( int argc, str argv[] )
+int main( int argc, char *argv[] )
 {
     return 0;
 }
 " > "src/main.c"
 
-echo "
-// Aliases
+echo '
+// Zed Shaw Debug Macros with colour
 
-typedef enum { false, true } bool;
-typedef unsigned int uint;
-typedef char* str;
+#ifndef __dbg_h__
+#define __dbg_h__
 
-" > "include/Aliases.h"
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#if !defined(_WIN32) && !defined(_WIN64)
+    #define TERM_RED      "\033[31m" // Error
+    #define TERM_BLUE     "\033[34m" // Debug
+    #define TERM_YELLOW   "\033[33m" // Warning
+    #define TERM_GREEN    "\033[32m"
+    #define TERM_GRAY     "\033[36m"
+    #define TERM_RESET    "\033[0m"
+#else
+    #define TERM_RED     ""
+    #define TERM_BLUE    ""
+    #define TERM_YELLOW  ""
+    #define TERM_GREEN   ""
+    #define TERM_GRAY    ""
+    #define TERM_RESET   ""
+#endif
+
+
+#ifdef NDEBUG
+    #define debug( M, ... )
+
+#else
+    #define debug( M, ... ) fprintf( stderr, "%s[DEBUG]%s (%s:%d)%s " M "\n%s",\
+        TERM_BLUE, TERM_GREEN, __FILE__, __LINE__, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+    #endif
+
+    #define clean_errno() ( errno == 0 ? "None" : strerror(errno) )
+
+    #define log_err( M, ... ) fprintf( stderr,\
+        "%s[ERROR]%s (%s:%d: errno: %s)%s " M "\n%s", TERM_RED, TERM_GREEN, __FILE__, __LINE__,\
+        clean_errno(), TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+
+    #define log_warn( M, ... ) fprintf(stderr,\
+        "%s[WARN]%s (%s:%d: errno: %s)%s " M "\n%s", TERM_YELLOW, TERM_GREEN, __FILE__, __LINE__,\
+        clean_errno(), TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+
+    #define log_info( M, ... ) fprintf( stderr, "%s[INFO]%s (%s:%d)%s " M "\n%s",\
+        TERM_BLUE, TERM_GREEN, __FILE__, __LINE__, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+
+    #define check( A, M, ... ) if( !(A) ) {\
+        log_err( M, ##__VA_ARGS__ ); errno=0; goto error; }
+
+    #define sentinel( M, ... ) { log_err( M, ##__VA_ARGS__ );\
+        errno=0; goto error; }
+
+    #define check_mem( A ) check( (A), "Out of memory." )
+
+    #define check_debug( A, M, ... ) if( !(A) ) { debug( M, ##__VA_ARGS__ );\
+        errno=0; goto error; }
+
+#endif
+' > "include/Debug.h"
 
 
 #-CMake Files-------------------------------------------------------------------
