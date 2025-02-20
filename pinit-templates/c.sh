61de7319a525034f7234b9cde7c34148376ffb0b
karsh <utkarsh@cyberfear.com>
2025-02-20 17:06:36 +1100

    feat: fork zed shaw debug
    ~ updated the conditions for different macros for production

--- pinit-templates/c.sh
+++ pinit-templates/c.sh
@@ -21,10 +21,11 @@ int main( int argc, char *argv[] )
 " > "src/main.c"
 
 echo '
-// Zed Shaw Debug Macros with colour
+// Fork of Zed Shaw debug macro
 
-#ifndef __dbg_h__
-#define __dbg_h__
+#pragma once
+
+// #define PRODUCTION
 
 #include <stdio.h>
 #include <errno.h>
@@ -46,18 +47,26 @@ echo '
     #define TERM_RESET   ""
 #endif
 
+#define clean_errno() ( errno == 0 ? "None" : strerror(errno) )
 
-#ifdef NDEBUG
+#ifdef PRODUCTION
     #define debug( M, ... )
+
+    #define log_error( M, ... ) fprintf( stderr, "%s[ERROR]%s " M "\n%s",\
+        TERM_RED, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+
+    #define log_warn( M, ... ) fprintf(stderr, "%s[WARN]%s " M "\n%s",\
+        TERM_YELLOW, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
+
+    #define log_info( M, ... ) fprintf( stderr, "%s[INFO]%s " M "\n%s",\
+        TERM_BLUE, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
 #else
     #define debug( M, ... ) fprintf( stderr, "%s[DEBUG]%s (%s:%d)%s " M "\n%s",\
         TERM_BLUE, TERM_GREEN, __FILE__, __LINE__, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
-    #endif
 
-    #define clean_errno() ( errno == 0 ? "None" : strerror(errno) )
 
-    #define log_err( M, ... ) fprintf( stderr,\
+    #define log_error( M, ... ) fprintf( stderr,\
         "%s[ERROR]%s (%s:%d: errno: %s)%s " M "\n%s", TERM_RED, TERM_GREEN, __FILE__, __LINE__,\
         clean_errno(), TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
@@ -68,18 +77,12 @@ echo '
     #define log_info( M, ... ) fprintf( stderr, "%s[INFO]%s (%s:%d)%s " M "\n%s",\
         TERM_BLUE, TERM_GREEN, __FILE__, __LINE__, TERM_GRAY, ##__VA_ARGS__, TERM_RESET )
 
-    #define check( A, M, ... ) if( !(A) ) {\
-        log_err( M, ##__VA_ARGS__ ); errno=0; goto error; }
-
-    #define sentinel( M, ... ) { log_err( M, ##__VA_ARGS__ );\
-        errno=0; goto error; }
-
-    #define check_mem( A ) check( (A), "Out of memory." )
-
-    #define check_debug( A, M, ... ) if( !(A) ) { debug( M, ##__VA_ARGS__ );\
-        errno=0; goto error; }
-
 #endif
+
+#define check( A, M, ... ) if( !(A) ) { log_error( M, ##__VA_ARGS__ ); errno=0; goto error; }
+#define sentinel( M, ... ) { log_error( M, ##__VA_ARGS__ ); errno=0; goto error; }
+#define check_mem( A ) check( (A), "Out of memory." )
+#define check_debug( A, M, ... ) if( !(A) ) { debug( M, ##__VA_ARGS__ ); errno=0; goto error; }
 ' > "include/Debug.h"
 
 
